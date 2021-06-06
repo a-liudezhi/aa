6f73fec026a9a5b00d39218e4b72565c85865aa7
liudezhi <a.liudezhi@gmail.com>
2021-06-06 12:18:38 +0800

    vote

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :topics
+  resources :topics do
+    member do
+      post 'upvote'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
 root "topics#index"
+
 end
